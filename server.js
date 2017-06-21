63f7a39b4db4c8cda53a0729e86ca6f113fa19ac
Shehan Gamage <shehangamage55@gmail.com>
2017-06-22 02:16:06 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+//setup partials-------------
+hbs.registerPartials(__dirname+'/views/partials');
+//set template engine------------
+app.set('view engine', 'hbs');
+//set helper ----------------
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text)=>{
+  return text.toUpperCase();
+});
+//use middleware -------------
+app.use((req, res, next)=>{
+  var date = new Date().toString();
+  var logTxt = `${date}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', logTxt+'\n', (error)=>{
+    if(error){
+      console.log('Unable to append to server.log.');
+    }
+  });
+  console.log(logTxt);
+  next();
+});
+//maintenance mood --------------
+// app.use((req, res, next)=>{
+//   res.render('maintenance.hbs');
+// });
+//set static contents------------
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', (req, res)=>{
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    welcomeMessage:"hi express users!"
+  });
+});
+
+app.get('/json', (req, res)=>{
+//  res.send('<h1>Hello Express!</h1>');
+    res.send({
+      name:'ෂෙහාන් ගමගේ',
+      languages:[
+        'Java',
+        'JavaScript',
+        'C#'
+      ],
+      frameworks:[
+        'Spring',
+        'NodeJs',
+        'AngularJs'
+      ]
+    });
+
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about.hbs',{
+      pageTitle: 'අප ගැන'
+    });
+});
+
+app.listen(3000, ()=>{
+  console.log('Server is run on port 3000');
+});
