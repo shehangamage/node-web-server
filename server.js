482b1e38612eab4c76796e4ded7e2c22b0a5ab18
Shehan Gamage <shehangamage55@gmail.com>
2017-06-22 13:55:06 +0530

    add env.PORT for deploy on Heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+//set port dynmically
+const port = process.env.PORT || 3000;
 var app = express();
 //setup partials-------------
 hbs.registerPartials(__dirname+'/views/partials');
@@ -64,6 +66,6 @@ app.get('/about', (req, res)=>{
     });
 });
 
-app.listen(3000, ()=>{
-  console.log('Server is run on port 3000');
+app.listen(port, ()=>{
+  console.log(`Server is run on port ${port}`);
 });
